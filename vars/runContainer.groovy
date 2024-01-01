b2f9f5b60f0e8036fee317493b881e75234f178a
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2024-01-01 18:18:27 +0530

    Update runContainer.groovy

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,3 +1,4 @@
 def call(){
-    sh "docker run -d --name youtube1 -p 3000:3000 sevenajay/youtube:latest"
+    sh "docker run -d --name youtube1 -p 3000:3000 anrs125/youtube:latest"
+    sh "docker run -d --name youtube2 -p 80:8080 anrs125/youtube:latest"
 }
